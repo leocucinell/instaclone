bbd41f4eec205fcc823d5b16f697a96e1fc69db7
Leo Cucinell <leocucinell@gmail.com>
2021-07-21 14:51:15 -0700

    stubbed all get requests in server

--- server.js
+++ server.js
@@ -13,19 +13,31 @@ const PORT = 4000;
 /* SECTION: Middleware */
 
 /* SECTION: Routes */
+app.get("/", (req, res) => {
+    res.redirect("/posts")
+});
 
 //Index GET / - Presentational
-app.get("/", (req, res) => {
+app.get("/posts", (req, res) => {
     res.send("Post Index");
 });
 
-// New GET /posts - Presentational form
+// New GET /posts/new - Presentational form
+app.get("/posts/new", (req, res) => {
+    res.send("Post Create Page");
+});
 
 // Create POST /posts - Functional
 
 // Show GET /posts/:id - Presentational
+app.get("/posts/:id", (req, res) => {
+    res.send(`Post show page id: ${req.params.id}`);
+});
 
 // Edit GET /posts/:id/edit - Presentational form
+app.get("/posts/:id/edit", (req, res) => {
+    res.send(`Post edit page for post with id: ${req.params.id}`);
+});
 
 // Update PUT /posts/:id - Functional
 
